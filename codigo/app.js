2097483a3a54eb47b22c1226e464b81a43387b3e
fray osorio <frayosorio@gmail.com>
2025-07-23 15:14:06 -0500

    Se ajusta el mensaje de verifición de grupos de cartas

--- codigo/app.js
+++ codigo/app.js
@@ -1,10 +1,15 @@
+let repartio = false;
+let cartas = [];
+
 function repartir() {
     let tablero = document.getElementById("tablero");
-    tablero.innerHTML="";
+    tablero.innerHTML = "";
     const TOTAL_CARTAS = 10;
+    cartas = [];
     for (let i = 0; i < TOTAL_CARTAS; i++) {
         let carta = document.createElement("img");
         let indice = Math.floor(Math.random() * 52) + 1;
+        cartas.push(indice);
         carta.src = "imagenes/cartas/Carta" + indice + ".jpg";
         tablero.appendChild(carta);
     }
@@ -12,9 +17,39 @@ function repartir() {
     audio.play().then(() => {
         window.alert("Se han repartido las 10 cartas");
     });
-
+    repartio = true;
 }
 
-function verificar(){
-    window.alert("Mañana chequearemos las cartas");
+function verificar() {
+    if (!repartio) {
+        window.alert("No se han repartido");
+    }
+    else {
+        //iniciar los contadores en 0
+        contadores = new Array(13).fill(0);
+        //recorrer la lista de las cartas
+        for (let i = 0; i < cartas.length; i++) {
+            let posicion = cartas[i] % 13;
+            if (posicion == 0) {
+                posicion = 12;
+            }
+            else {
+                posicion--;
+            }
+            contadores[posicion]++;
+        }
+        nombreCartas = ["As", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho",
+            "Nueve", "Diez", "Jack", "Queen", "King"];
+        //recorrer los contadores
+        let mensaje = "Se encontraron los siguientes grupos:\n";
+        grupos = ["Vacío", "Non", "Par", "Terna", "Cuarta", "Quinta", "Sexta", 
+            "Séptima", "Octava", "Novena", "Décima"];
+        for (let i = 0; i < contadores.length; i++) {
+            if (contadores[i] >= 2) {
+                mensaje += grupos[contadores[i]] + " de " + nombreCartas[i] + "\n";
+            }
+        }
+        window.alert(mensaje);
+    }
 }
+
